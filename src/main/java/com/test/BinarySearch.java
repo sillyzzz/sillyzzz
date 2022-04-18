eb170649529fdd66b8ba226316ef01f70f78f5a9
忧郁小王子 <857615027@qq.com>
2022-04-18 23:39:13 +0800

    冒泡排序

--- src/main/java/com/test/BinarySearch.java
+++ src/main/java/com/test/BinarySearch.java
@@ -1,6 +1,17 @@
 package com.test;
-
+/*
+* 前提：
+* 1.有已排序数组A(假设已经做好)
+* 2.定义左边界L、右边界R，确定搜索范围，循环执行二分查找（3、4两步）
+* 3.获取中间索引的值A[M]=Floor((L+R)/2)
+* 4.中间索引值A[M]与待搜索的值T进行比较
+* a.A[M]=T表示找到，返回中间索引
+* b.A[M]>T,中间值右侧的其它元素都大于T，无需比较，中间索引左边去找，M-1设置为右边界，重新查找
+* c.A[M]>T,中间值左侧的其它元素都小于T，无需比较，中间索引右边去找，M-1设置为右边界，重新查找
+* 5.当L>R时，表示没有找到，应结束循环*/
 public class BinarySearch {
+    /*奇数二分取中间
+    * 偶数二分取中间靠左*/
     public static void main(String[] args) {
         int[] arr = {1,5,8,11,19,22,31,35,40,45,48,49,50};
         int target = 40;
@@ -20,9 +31,11 @@ public class BinarySearch {
         int l = 0,r=a.length-1,m;
         /*左边界小于右边界*/
         while (l <= r) {
-            m = (l+r)/2;
-            System.out.println((l+r));
+//            m = (l+r)/2;
+            /*右移算法*/
+            m = (l+r) >>> 1;
             System.out.println((l+r)/2);
+            System.out.println((l+r) >>> 1);
             if(a[m] == t){
                 return m;
             }else if(a[m]>t){
--- src/main/java/com/test/BubbleSort.java
+++ src/main/java/com/test/BubbleSort.java
@@ -1,4 +1,70 @@
 package com.test;
 
-public class BubbleSearch {
+import java.util.Arrays;
+
+/*文字描述：
+1.依次比较数组中相邻两个元素大小，若a[j]>a[j+1],则交换两个元素，两两都比较一遍称为一轮冒泡，结果是让最大的元素排至最后
+* 2.重复以上步骤，直到整个数组有序
+优化方式：
+每轮冒泡时，最后一次交换索引可以作为下一轮冒泡的比较次数，如果这个值为0，表示整个数组有序，直接退出外层循环即可
+
+*/
+
+public class BubbleSort {
+    public static void main(String[] args) {
+//        int [] a ={5,9,7,4,1,3,2,8};
+        int [] a ={1,2,3,4,5,6,7,9};
+//        int [] a ={5,2,7,4,1,3,8,9};
+//        bubble(a);
+        bubble_v2(a);
+    }
+
+    private static void bubble_v2(int[] a) {
+        int n = a.length-1;
+        while (true) {
+            //表示最后一次交换索引位置
+            int last = 0;
+            for (int i = 0; i <n; i++) {
+                System.out.println("比较次数" + i);
+                if (a[i] > a[i+1]) {
+                    swap(a,i,i+1);
+                    last = i;
+                }
+            }
+            n = last;
+            System.out.println("第"+"轮冒泡"+ Arrays.toString(a));
+            if (n==0) {
+                break;
+            }
+        }
+
+    }
+
+
+    private static void bubble(int[] a) {
+        for (int j = 0; j <a.length-1 ; j++) {
+            //一轮冒泡
+            //是否发生了交换
+            boolean swapped = false;
+            for (int i = 0; i < a.length-1-j; i++) {
+                System.out.println("比较次数" + i);
+                if (a[i] > a[i+1]) {
+                    swap(a,i,i+1);
+                    swapped = true;
+                }
+            }
+            System.out.println("第"+j+"轮冒泡"+ Arrays.toString(a));
+            if (!swapped){
+                break;
+            }
+        }
+
+
+    }
+
+    public static void swap (int[] a,int i,int j){
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
 }
